47ffa9be48b9e0759248565cdc9da17d6f68b749
RVPdev <renatovilcahuaman@gmail.com>
2023-11-20 22:49:10 -0600

    infor about variable

--- Hello_World.java
+++ Hello_World.java
@@ -1,8 +1,9 @@
 class Main {
+    static int age = 27; // larger scope able to be used in the entire main class
+    // static int age; If you dont declare the value in the global scope Java will do it automatically to 0
     public static void main(String[] args) {
-        int age;
-
-        age = 25;
+        // int age; // local vatialbe
+        // age = 25; // variable decalration || also assing new value to the age variable inside a method
 
         // Also
         // int age = 27;
