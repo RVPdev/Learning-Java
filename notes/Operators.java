b802d80c44b644515eef94dd9f89dc467fd566bb
RVPdev <renatovilcahuaman@gmail.com>
2023-11-21 14:40:15 -0600

    added operators

---
+++ notes/Operators.java
@@ -0,0 +1,24 @@
+package notes;
+
+class Operators {
+    public static void main(String[] args) {
+        int number1 = 12;
+        int number2 = 6;
+
+        // addition
+        System.out.println(number1 + number2); // 18
+
+        // subs
+        System.out.println(number1 - number2); // 6
+
+        // multiplication
+        System.out.println(number1 * number2); // 72
+
+        // division
+        System.out.println(number1 / number2); // 2
+
+        // remainder (modulus)
+        System.out.println(number1 % number2); // 0
+
+    }
+}
