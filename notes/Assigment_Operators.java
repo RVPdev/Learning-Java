6aa5536320eb05a3ec9c6fc2a1838e6a509e981a
RVPdev <renatovilcahuaman@gmail.com>
2023-11-21 21:41:57 -0600

    assisgment operators

---
+++ notes/Assigment_Operators.java
@@ -0,0 +1,26 @@
+package notes;
+
+public class Assigment_Operators {
+    public static void main(String[] args) {
+        int number1 = 12;
+        int number2 = 15;
+
+        // is equal
+        System.out.println(number1 == number2);
+
+        // is not equal
+        // System.out.println(number1 != number2);
+
+        // is greater than
+        // System.out.println(number1 > number2);
+
+        // is less than
+        // System.out.println(number1 < number2);
+
+        // is greater than or equal to
+        // System.out.println(number1 >= number2);
+
+        // is less than or equal to
+        // System.out.println(number1 <= number2);
+    }
+}
