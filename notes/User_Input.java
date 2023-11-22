36d7c87fc74c4e567af019bcb4aeac5d7a79a208
RVPdev <renatovilcahuaman@gmail.com>
2023-11-22 15:14:17 -0600

    extra sceurity on inputs

--- notes/User_Input.java
+++ notes/User_Input.java
@@ -7,13 +7,19 @@ public class User_Input {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Whats your name? \n"); //! println vs print, println provides new line print does not "\n" means new line
-        String name = scanner.nextLine();
+        String name = scanner.nextLine(); // * takes input plus new line
 
         System.out.printf("Hello %s. How are you?", name); // * printf allows formatting
         
-        int age = scanner.nextInt();
+        int age = scanner.nextInt(); // ! next int just takes input, not new line and the new line stays in the buffer
+
+        scanner.nextLine(); // * input buffer for security, clears the new line from the next int
         
         System.out.printf("%d is an execllent age to start coding.", age); // * printf allows formatting
+        
+        String language = scanner.nextLine(); //! get the newline form the buffer skipping your input
+        
+        System.out.printf("%s is a very popular language.", language); //! this will print straigth out without taking input, 
 
         scanner.close(); //! remember to close the scanner once done with it
     }
